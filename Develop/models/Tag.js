5d0246dd6eebc4a60da67e100258ba01f98f8cd8
ayandele <adele.ayansola@yahoo.com>
2023-07-10 16:50:00 -0400

    code updates to files

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -1,20 +1,32 @@
+// Import important parts of the Sequelize library
 const { Model, DataTypes } = require('sequelize');
+// Import the database connection from config.js
+const sequelize = require('../config/connection');
 
-const sequelize = require('../config/connection.js');
-
+// Initialize Tag model (table) by extending off Sequelize's Model class
 class Tag extends Model {}
 
+// Set up fields and rules for Tag model
 Tag.init(
-  {
-    // define columns
-  },
-  {
+{
+    id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+    },
+    tag_name: {
+    type: DataTypes.STRING,
+    allowNull: false
+    }
+},
+{
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'tag',
-  }
+    modelName: 'tag'
+}
 );
 
 module.exports = Tag;
